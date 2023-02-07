fa63e45f42a4815f827104439a1f8bfcf923d00e
Mar Test <mmatvienko111@gmail.com>
2023-02-07 23:51:51 +0300

    Test failed

---
+++ src/test/java/ru/netology/test.java
@@ -0,0 +1,49 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class test {
+    @Test
+    public void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("25/12/1989") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("25/12/1989"));
+    }
+
+    @Test
+    void shouldReturnDemoAccounts1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:2")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("id:4"));
+    }
+
+    @Test
+    void shouldReturnDemoAccounts3() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:RUB")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency:RUB"));
+    }
+
+}
